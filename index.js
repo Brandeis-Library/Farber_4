3f4484b6af8e0b862868a2a21b06b11dd835519a
Chris Underwood <cunderwoodmn@gmail.com>
2020-11-03 16:21:00 -0500

    start gathering information needed for the project. Create API KEY and add it to the .env file.

---
+++ index.js
@@ -0,0 +1,9 @@
+// URL for sandbox
+// https://brandeis-psb.alma.exlibrisgroup.com/mng/login?auth=SAML
+
+// API to be used is scan-in under the Bib Records & Inventory section
+// Documentation
+// https://developers.exlibrisgroup.com/alma/apis/docs/bibs/UE9TVCAvYWxtYXdzL3YxL2JpYnMve21tc19pZH0vaG9sZGluZ3Mve2hvbGRpbmdfaWR9L2l0ZW1zL3tpdGVtX3BpZH0=/
+
+// Sandbox API KEY
+// process.env.SANDBOX_SCAN_IN_API_KEY
