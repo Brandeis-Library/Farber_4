c46afeef6cbea921d0edf09b3797464884d105ff
Chris Underwood <cunderwoodmn@gmail.com>
2020-11-04 21:22:44 -0500

    finalized round 1 of the params needed for the post operation

--- index.js
+++ index.js
@@ -5,5 +5,43 @@
 // Documentation
 // https://developers.exlibrisgroup.com/alma/apis/docs/bibs/UE9TVCAvYWxtYXdzL3YxL2JpYnMve21tc19pZH0vaG9sZGluZ3Mve2hvbGRpbmdfaWR9L2l0ZW1zL3tpdGVtX3BpZH0=/
 
+// Sandbox URL
+// https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs/{mms_id}/holdings/{holding_id}/items/{item_pid}
+
 // Sandbox API KEY
 // process.env.SANDBOX_SCAN_IN_API_KEY
+
+// URL paramas
+
+// mms_id
+// per record
+
+// holding_id
+// per record
+
+// item_pid
+// per record
+
+// op
+// scan (only option)
+
+// external_id
+// false  (true or false)
+
+// request_id
+// Not needed.
+
+// library
+//  MAIN
+
+// circ desk
+//  No circ desk for this search
+
+// despartment
+// PRESDEPT
+
+// work_order_type
+// PRES
+
+// status
+// 05VENDOR
